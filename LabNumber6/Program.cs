b19e6d83c0d67a0c90de13da6a7fb0a1be9f37d0
Aleena Tucker <Aleena@DESKTOP-N857AIT>
2016-11-02 16:45:18 -0400

    Add project files.

---
+++ LabNumber6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LabNumber6
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            char[] vowels = "AEIOUaeiou".ToCharArray();
+            StringBuilder pigLatin = new StringBuilder();
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
+            Console.WriteLine();
+            Console.Write("Enter a line to be translated: ");
+
+            string[] sentenceArray = GetInput().Split(' ');
+            TranslateWords(sentenceArray, vowels);
+        }
+
+        private static void TranslateWords(string[] sentenceArray, char[] vowels)
+        {
+            foreach (string word in sentenceArray)
+            {
+                if (vowels.Contains(word[0]))
+                {
+                    Console.Write(word + "way ");
+                }
+                else
+                {
+                    Console.Write(word.Remove(0, 1) + word[0] + "ay ");
+                }
+            }
+        }
+
+        static string GetInput()
+        {
+            string input = Console.ReadLine();
+            input.Trim();      
+            return input;
+        }
+    }
+}
